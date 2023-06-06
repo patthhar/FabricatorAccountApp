503204924409f5aa2649c6f2de20cda1c0388cb0
Parth Takkar <parthtakkar@gmail.com>
2023-06-06 17:46:24 +0530

    Changed slips to mutableLists in dashboardState

--- app/src/main/java/me/darthwithap/android/fabricatoraccount/presentation/dashboard/DashboardState.kt
+++ app/src/main/java/me/darthwithap/android/fabricatoraccount/presentation/dashboard/DashboardState.kt
@@ -4,15 +4,14 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import me.darthwithap.android.fabricatoraccount.domain.models.FromFabricatorSlip
 import me.darthwithap.android.fabricatoraccount.domain.models.ToFabricatorSlip
-import me.darthwithap.android.fabricatoraccount.domain.usecases.dashboard.GetWeekDates
 import java.time.LocalDate
 
 @RequiresApi(Build.VERSION_CODES.O)
 data class DashboardState(
   val selectedDate: LocalDate = LocalDate.now(),
   val weekDates: List<LocalDate> = emptyList(),
-  val todaySlips: List<FromFabricatorSlip> = emptyList(),
-  val activeSlips: List<ToFabricatorSlip> = emptyList(),
+  val slipsForDate: MutableList<FromFabricatorSlip> = mutableListOf(),
+  val activeSlips: MutableList<ToFabricatorSlip> = mutableListOf(),
   //TODO: add fabricator balances functionality
   //val fabricatorBalances: Map<Fabricator, Int>
 )
